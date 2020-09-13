2007585a4e01ed19fe45af1be629d3aa3b6caeb2
bitlucky <lokeshtiwari877@gmail.com>
2020-09-14 02:57:43 +0530

    Header Component

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,33 @@
-import React from 'react';
+import React from "react";
 import { Avatar } from "@material-ui/core";
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import HelpIcon from '@material-ui/icons/Help';
-import SearchIcon from '@material-ui/icons/Search';
-import "./Header.css"
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SearchIcon from "@material-ui/icons/Search";
+import "./Header.css";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className = "header_left"> 
-                {/* Avatars for logged in user */}
-                <Avatar 
-                className = "header_avatar"
-                alt = "rMr. Reflex"
-                src=''
-                />
-                {/* Time icon */}
-                <AccessTimeIcon/>
-            </div>
+  return (
+    <div className="header">
+      <div className="header_left">
+        {/* Avatars for logged in user */}
+        <Avatar className="header_avatar" alt="rMr. Reflex" src="" />
+        {/* Time icon */}
+        <AccessTimeIcon />
+      </div>
 
-            <div className = "header_right">
-            {/* Search Icon  */}
-            <SearchIcon/>
-            {/* Input  */}
-            </div>
+      <div className="header_right">
+        {/* Search Icon  */}
+        <SearchIcon />
+        <input placeholder = "Search Friends"/>
+        {/* Input  */}
+      </div>
 
-            <div className = "header_right">
-                {/* Help icon  */}
-                <HelpIcon/>
-
-            </div>
-
-        </div>
-    )
+      <div className="header_right">
+        {/* Help icon  */}
+       <HelpOutlineIcon/>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
