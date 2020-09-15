1e04d983a4d6cb114e492c583d81f47228b4b539
bitlucky <lokeshtiwari877@gmail.com>
2020-09-16 04:57:43 +0530

    Option added to sidebar component'

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import CreateIcon from '@material-ui/icons/Create';
+import SideBarOption from "./SideBarOption";
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+
 function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <h2> Hello lokesh tiwari </h2>
+        <div className = "sidebar__info">
+        <h2>REFLEX </h2>
         <h3>
           <FiberManualRecordIcon />
           Lokesh Tiwari
         </h3>
+        
+        </div>
+        <CreateIcon/>
+        
       </div>
+      <SideBarOption Icon= {InsertCommentIcon} title = "Threads" />
+      <SideBarOption  title = "Youtube" />
     </div>
   );
 }
